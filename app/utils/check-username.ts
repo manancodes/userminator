094782d5a3ea1c1f2dfbf355915ddaee2e8fe639
Oscar <5xp395ab@duck.com>
2023-10-31 10:23:20 +0000

    feat: add Github username check

--- app/utils/check-username.ts
+++ app/utils/check-username.ts
@@ -1,16 +1,36 @@
+// false -> username taken / true -> username available
 export type UsernameAvailability = {
     github: boolean;
     // More to come...
 }
 
 export async function checkAvailability(username: string): Promise<UsernameAvailability> {
-    let res: UsernameAvailability = {
-        github: await checkGithubAvailability(username)
+    // Run all external queries in parallel
+    const [github] = await Promise.all([
+        checkGithubAvailability(username)
+    ])
+
+    const res: UsernameAvailability = {
+        github: github
     }
 
     return res;
 }
 
 async function checkGithubAvailability(username: string) {
-    return true;
+    const res = await fetch("https://api.github.com/users/" + username, {
+        headers: {
+            "Accept": "application/vnd.github+json",
+            "Authorization": "Bearer " + process.env.GITHUB_TOKEN,
+            "X-GitHub-Api-Version": "2022-11-28"
+        }
+    })
+
+    if (res.status == 200) {
+        // username is taken
+        return false;
+    } else {
+        // username is available
+        return true;
+    }
 }
